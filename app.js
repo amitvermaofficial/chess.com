d7f89f358990e0ba2f3847a33ef954c33a756ca9
amitvermaofficial <amitvns159@gmail.com>
2025-04-12 11:38:11 +0530

    chess.com

--- app.js
+++ app.js
@@ -125,6 +125,8 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, (err) => {
+    if (err) {
+        console.log(err.message);
+    }
 });
